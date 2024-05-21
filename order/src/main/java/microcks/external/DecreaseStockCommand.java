30e2658fd3f1f881f129ed60b3e306ac6bd7976d
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2024-05-21 14:34:37 +0900

    commit

---
+++ order/src/main/java/microcks/external/DecreaseStockCommand.java
@@ -0,0 +1,13 @@
+package microcks.external;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+public class DecreaseStockCommand {
+
+    private Long id;
+    private Integer qty;
+}
